e043af078a7257d9125c5f614614c43a6a1278ba
Khinshan Khan <khanshan501@gmail.com>
2020-01-08 14:43:15 -0500

    Merged files, fixed errors to start project

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,29 +1,80 @@
-import React from 'react';
-import Display from './Display'
-import Input from './Input'
-import '../App.css';
+import React from "react";
+import "../App.css";
 
-class Form extends React.Component {
+export default class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: 'David',
-      lastName: 'Mitchell'
+      firstName: "David",
+      lastName: "Mitchell"
     };
-      this.handleNameChange = this.handleNameChange.bind(this);
-    }
-    render() {
-      return <div>
-      <Display/>
-      <Input/>
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  Display = () => {
+    return (
+      <div>
+        <div>First Name: {this.state.firstName}</div>
+        <div>Last Name: {this.state.lastName}</div>
+        <button type="button" onClick={this.Edit}>
+          Edit
+        </button>
       </div>
-    } 
+    );
+  };
 
-    handleNameChange(event) {
-      this.setState({
-        name: event.target.value
-      });
-    }
-};
+  Input = () => {
+    return (
+      <div>
+        <input
+          type="text"
+          id="firstName"
+          placeholder={this.state.firstName}
+          onChange={this.handleFirstNameChange}
+        />
+        <input
+          type="text"
+          id="lastName"
+          placeholder={this.state.lastName}
+          onChange={this.handleLastNameChange}
+        />
+        <button type="button" onClick={this.Save}>
+          Save
+        </button>
+        <button type="button" onClick={this.Cancel}>
+          Cancel
+        </button>
+      </div>
+    );
+  };
 
-export default Form;
+  handleFirstNameChange = event => {
+    this.setState({
+      firstName: event.target.value
+    });
+  };
+
+  handleLastNameChange = event => {
+    this.setState({
+      lastName: event.target.value
+    });
+  };
+
+  Save = () => {};
+  Cancel = () => {};
+
+  render() {
+    return (
+      <div>
+        {this.Display()}
+        {this.Input()}
+      </div>
+    );
+  }
+
+  handleNameChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+}

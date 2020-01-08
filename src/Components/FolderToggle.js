33b866e5d7c61ff7a3338ea987076480e6228e4f
Samson <samson50wu@gmail.com>
2020-01-08 15:49:23 -0500

    Added folder toggle

--- src/Components/FolderToggle.js
+++ src/Components/FolderToggle.js
@@ -9,11 +9,28 @@ export default class FolderToggle extends React.Component {
     };
   }
 
+  toggleFolders = () => {
+    this.setState({showFolders: !this.state.showFolders})
+  }
+
   render() {
     if (this.state.showFolders) {
-      return <div>yay</div>;
+      return <div>
+        <h2>Home</h2>
+        <div>
+          <ul>
+            <li>File1</li>
+            <li>File2</li>
+            <li>File3</li>
+          </ul>
+        </div>
+        <button onClick={this.toggleFolders}>Toggle</button>
+      </div>;
     } else {
-      return <div>woo</div>;
+      return <div>
+        <h2>Home</h2>
+        <button onClick={this.toggleFolders}>Toggle</button>
+      </div>;
     }
   }
 }
